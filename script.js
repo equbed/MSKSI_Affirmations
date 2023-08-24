838a8c47b4ccb50767b81e3b100a929559acb672
equbed <142814969+equbed@users.noreply.github.com>
2023-08-24 17:29:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
         "Quote 1",
         "Quote 2",
         "Quote 3",
+        "Life is 10% what happens to you and 90% how you react to it. - Charles R. Swindoll",
+        "Change your thoughts, and you change your world.- Norman Vincent Peale",
         // Add more quotes here
     ];
     const quoteElement = document.getElementById("quote");
