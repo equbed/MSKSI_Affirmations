24d299ada7c16bd965b092b8819964cb95d2d945
equbed <142814969+equbed@users.noreply.github.com>
2023-08-24 12:43:28 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -15,5 +15,6 @@ var quotes = [
   ]
 
 function newQuote() {
-  var randomNumber = Math.floor(Math.random()*(quotes.length));
+  var randomNumber = Math.floor(Math.random() * (quotes.length));
   document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber};
+}
