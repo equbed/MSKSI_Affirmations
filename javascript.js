e984cb681cb57e8aaedbb82978a71f8999b81445
equbed <142814969+equbed@users.noreply.github.com>
2023-08-24 12:45:03 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,12 +3,12 @@ var quotes = [
   'Strive not to be a success, but rather to be of value. –Albert Einstein'
   'Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.  –Robert Frost'
   'I attribute my success to this: I never gave or took any excuse. –Florence Nightingale'
-  'You miss 100% of the shots you don’t take. –Wayne Gretzky'
+  'You miss 100% of the shots you don\’t take. –Wayne Gretzky'
   'The most difficult thing is the decision to act, the rest is merely tenacity. –Amelia Earhart'
   'Every strike brings me closer to the next home run. –Babe Ruth'
   'Definiteness of purpose is the starting point of all achievement. –W. Clement Stone'
-  'Life isn't about getting and having, it's about giving and being. –Kevin Kruse'
-  'Life is what happens to you while you’re busy making other plans. –John Lennon'
+  'Life isn\'t about getting and having, it\'s about giving and being. –Kevin Kruse'
+  'Life is what happens to you while you\’re busy making other plans. –John Lennon'
   'We become what we think about. –Earl Nightingale'
 
   ]
