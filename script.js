48d083162587aa03eb2d48068d3916efca122fef
equbed <142814969+equbed@users.noreply.github.com>
2023-08-24 16:55:52 -0400

    Update script.js
    Chat GPT starting point

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-const fetchData = () => {
-    fetch("https://script.googleusercontent.com/macros/echo?user_content_key=WyeSgqX2SKOpQy57z9VAQd-PgNBJOqGSV5NuwPZtV9PO-R_vOkokxbxN8N90S-Bw7cRLBCKxx567lV6NTjJ_LoJ-zelNJl9qm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnLxWCD8mtqwU9KXRvEAjYFjf5FN_mQ3tUpX9NoT6dASPjCZKffhSQhN9y8TRou0U8KFloFx5HaAwfpQ5nlGxXMwEoRqhr8JRU9z9Jw9Md8uu&lib=MQUw2r8KEuTjd3CTMhVcFmATRcQas6JuQ")
-        .then(response => response.json())
-        .then(data => displayQuote(data))
-}
+const quotes = [
+    "Quote 1",
+    "Quote 2",
+    "Quote 3",
+    // Add more quotes here
+];
 
-const displayQuote = data => {
-    const quoteElement = document.getElementById("quote");
-    quoteElement.innerText = data.quote;
-}
+const quoteElement = document.getElementById("quote");
+const generateButton = document.getElementById("generate-btn");
 
-// display quote when refresh a page
-fetchData();
-
-// generate new when btn click
-document.getElementById("btn-generate").addEventListener("click", fetchData);
+generateButton.addEventListener("click", () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    quoteElement.textContent = quotes[randomIndex];
+});
