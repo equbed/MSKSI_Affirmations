049885df8bfbfba22ea7a6ea03736ad86b474765
equbed <142814969+equbed@users.noreply.github.com>
2023-08-24 17:21:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
         "Quote 1",
         "Quote 2",
         "Quote 3",
-        "I learned that courage was not the absence of fear, but the triumph over it. The brave man is not he who does not feel afraid, but he who conquers that fear. Nelson Mandela",
-        "Change your thoughts, and you change your world.- Norman Vincent Peale",
-        "Success is a journey not a destination. - Ben Sweetland",
-        "What you get by achieving your goals is not as important as what you become by achieving your goals. - Zig Ziglar",
-        "It always seems impossible until it's done. - Nelson Mandela",
-        "Success is liking yourself, liking what you do, and liking how you do it. - Maya Angelou",
-        "Aim for the moon. If you miss, you may hit a star. - W. Clement Stone",
-        "If you cannot do great things, do small things in a great way. - Napoleon Hill",
-        "Success only comes to those who dare to attempt. - Mallika Tripathi",
-        "I never dreamed about success. I worked for it. - Estée Lauder",
-        "If opportunity doesn't knock, build a door. - Milton Berle"
-    
+        "I learned that courage was not the absence of fear, but the triumph over it. The brave man is not he who does not feel afraid, but he who conquers that fear. Nelson Mandela"
+           
         // Add more quotes here
     ];
 
