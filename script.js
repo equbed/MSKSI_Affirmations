4dc70315c7f68999f552cd8cfc32df2c0891404e
equbed <142814969+equbed@users.noreply.github.com>
2023-08-24 17:06:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-const quotes = [
-    "Quote 1",
-    "Quote 2",
-    "Quote 3",
-    "I learned that courage was not the absence of fear, but the triumph over it. The brave man is not he who does not feel afraid, but he who conquers that fear. Nelson Mandela"
-    // Add more quotes here
-];
+document.addEventListener("DOMContentLoaded", () => {
+    const quotes = [
+        "Quote 1",
+        "Quote 2",
+        "Quote 3",
+        "I learned that courage was not the absence of fear, but the triumph over it. The brave man is not he who does not feel afraid, but he who conquers that fear. Nelson Mandela"
 
-const quoteElement = document.getElementById("quote");
-const generateButton = document.getElementById("generate-btn");
+        // Add more quotes here
+    ];
 
-generateButton.addEventListener("click", () => {
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    quoteElement.textContent = quotes[randomIndex];
+    const quoteElement = document.getElementById("quote");
+    const generateButton = document.getElementById("generate-btn");
+
+    generateButton.addEventListener("click", () => {
+        const randomIndex = Math.floor(Math.random() * quotes.length);
+        quoteElement.textContent = quotes[randomIndex];
+    });
 });
+
+   
