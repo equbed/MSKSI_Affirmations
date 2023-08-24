3455cbecee7b06b221ce3af75508601d33b8d1a7
equbed <142814969+equbed@users.noreply.github.com>
2023-08-24 14:09:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-const generateQuote = () => {
-    let url = "https://type.fit/api/quotes";
-    fetch(url).then(function (response) {
-        return response.json()
-    }).then(function (data) {
-        let randNum = Math.floor((Math.random() * 1500) + 1);
-        let randomQuote = data[randNum];
-        document.getElementById("quote").innerHTML = `"${randomQuote.text}"`;
-        document.getElementById("author").innerHTML = `- ${randomQuote.author ? randomQuote.author : ""}`;
-    });
+const fetchData = () => {
+    fetch("https://api.kanye.rest/")
+        .then(response => response.json())
+        .then(data => displayQuote(data))
 }
+
+const displayQuote = data => {
+    const quoteElement = document.getElementById("quote");
+    quoteElement.innerText = data.quote;
+}
+
+// display quote when refresh a page
+fetchData();
+
+// generate new when btn click
+document.getElementById("btn-generate").addEventListener("click", fetchData);
