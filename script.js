ebd9c2f4e35dc902bc0a84623a4a49b40bbab261
equbed <142814969+equbed@users.noreply.github.com>
2023-08-24 16:37:38 -0400

    Update script.js
    My API

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const fetchData = () => {
-    fetch("AKfycbwnTDdba6QxrNVwSOWKDGK3j_dxjPwv0bQu9mfpJYvpAP3GK-p635mAhoR289rgPIP3jQ")
+    fetch("https://script.googleusercontent.com/macros/echo?user_content_key=WyeSgqX2SKOpQy57z9VAQd-PgNBJOqGSV5NuwPZtV9PO-R_vOkokxbxN8N90S-Bw7cRLBCKxx567lV6NTjJ_LoJ-zelNJl9qm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnLxWCD8mtqwU9KXRvEAjYFjf5FN_mQ3tUpX9NoT6dASPjCZKffhSQhN9y8TRou0U8KFloFx5HaAwfpQ5nlGxXMwEoRqhr8JRU9z9Jw9Md8uu&lib=MQUw2r8KEuTjd3CTMhVcFmATRcQas6JuQ")
         .then(response => response.json())
         .then(data => displayQuote(data))
 }
