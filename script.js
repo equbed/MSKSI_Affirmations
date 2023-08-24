cf915106c2bee22574099094ddf0bdf5b447f593
equbed <142814969+equbed@users.noreply.github.com>
2023-08-24 19:55:32 -0400

    Update script.js
    Added more quotes

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     const quotes = [
-        "Quote 1",
-        "Quote 2",
-        "Quote 3",
+        "Success is liking yourself, liking what you do, and liking how you do it. - Maya Angelou",
+        "Aim for the moon. If you miss, you may hit a star. - W. Clement Stone",
+        "If you cannot do great things, do small things in a great way. - Napoleon Hill:,
+        "Success only comes to those who dare to attempt. - Mallika Tripathi",
+        "I never dreamed about success. I worked for it. - Estée Lauder",
+        "If opportunity doesn't knock, build a door. - Milton Berle",
         "Change your thoughts and you change your world.- Norman Vincent Peale",
         "Success is a journey not a destination. - Ben Sweetland",
         "It always seems impossible until it's done. - Nelson Mandela",
