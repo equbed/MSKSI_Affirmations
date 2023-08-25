5ef313ac67878e7b574c78f41e5a6bfc2ad3853a
equbed <142814969+equbed@users.noreply.github.com>
2023-08-24 20:49:12 -0400

    Update script.js
    changed to image based

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-   const quotes = [
-    "Success is liking yourself, liking what you do, and liking how you do it. - Maya Angelou",
-    "Success only comes to those who dare to attempt. - Mallika Tripathi",
-    "Change your thoughts and you change your world. - Norman Vincent Peale",
-    "Success is a journey not a destination. - Ben Sweetland",
-    "It always seems impossible until it's done. - Nelson Mandela",
-      "Goal setting is the secret to a compelling future. — Tony Robbins",
-      "When we strive to become better than we are, everything around us becomes better too. — Paulo Coelho",
-      "Setting goals is the first step in turning the invisible into the visible. — Tony Robbins",      
-    // Add more quotes here
+   const quotes = [    
+      "Affirmations/1.gif",
+      "Affirmations/2.gif",
+      "Affirmations/3.gif",
+      "Affirmations/4.gif",
+
+     // Add more image URLs here
+
 ];
-    const quoteElement = document.getElementById("quote");
+        quoteElement.innerHTML = `<img src="${imageUrl}" alt="Quote Image">`;
     const generateButton = document.getElementById("generate-btn");
 
     generateButton.addEventListener("click", () => {
