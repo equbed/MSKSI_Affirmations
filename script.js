7dff834de3422edcdadb3ba7711cda5b4e67a73c
equbed <142814969+equbed@users.noreply.github.com>
2023-08-24 21:16:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ document.addEventListener("DOMContentLoaded", () => {
         "Affirmations/2.gif",
         "Affirmations/3.gif",
         "Affirmations/4.gif",
+        "Affirmations/5.gif",
+        "Affirmations/6.gif",
+        "Affirmations/7.gif",
+        "Affirmations/8.gif",
+        "Affirmations/9.gif",
+        "Affirmations/10.gif",
+        "Affirmations/11.gif",
+        "Affirmations/12.gif",
+        "Affirmations/13.gif",
+        "Affirmations/14.gif",
+        "Affirmations/15.gif",
+        "Affirmations/16.gif",
+        "Affirmations/17.gif",
+        "Affirmations/18.gif",
+        "Affirmations/19.gif",
         // Add more image URLs here
     ];
 
