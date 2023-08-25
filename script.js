0af714f8a8e1a0867e22b61dac02d977dc3869fe
equbed <142814969+equbed@users.noreply.github.com>
2023-08-24 20:52:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-const quoteElement = document.getElementById("quote");
-   const quotes = [    
-      "Affirmations/1.gif",
-      "Affirmations/2.gif",
-      "Affirmations/3.gif",
-      "Affirmations/4.gif",
+    const quotes = [
+        "Affirmations/1.gif",
+        "Affirmations/2.gif",
+        "Affirmations/3.gif",
+        "Affirmations/4.gif",
+        // Add more image URLs here
+    ];
 
-     // Add more image URLs here
-
-];
-        quoteElement.innerHTML = `<img src="${imageUrl}" alt="Quote Image">`;
+    const quoteElement = document.getElementById("quote");
+    const quoteImage = document.getElementById("quote-image");
     const generateButton = document.getElementById("generate-btn");
 
     generateButton.addEventListener("click", () => {
         const randomIndex = Math.floor(Math.random() * quotes.length);
-        quoteElement.textContent = quotes[randomIndex];
+        quoteImage.src = quotes[randomIndex];
     });
 });
